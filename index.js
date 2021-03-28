c9e8d0881b6a11635ca59a62885b9ad1261a7018
WalukaUI <cwaluka@yahoo.com>
2021-03-28 08:46:26 -0700

    eventlistner for SkillsDiV

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
-const input = document.querySelector('#main');
-const inputbut = document.querySelector('#aboutButton');
-
+const inputbutton = document.querySelector('.aboutButton');
 
 function showMe() {
   var maineDiv = document.getElementById('main');
- 
-  if(maineDiv.style.display == '' || maineDiv.style.display == 'none'){
+  if(maineDiv.style.display === '' || maineDiv.style.display === 'none'){
     maineDiv.style.display = 'block';
   }
   else {
     maineDiv.style.display = 'none';
   }
 }
+inputbutton.addEventListener('click', showMe);
 
-inputbut.addEventListener('click', showMe);
+const inputMySkills = document.querySelector('.mySkills');
 
+function showMeSkills() {
+  var skillsDiv = document.getElementById('skills');
+  if(skillsDiv.style.display === '' || skillsDiv.style.display === 'none'){
+    skillsDiv.style.display = 'block';
+  }
+  else {
+    skillsDiv.style.display = 'none';
+  }
+}
+inputMySkills.addEventListener('click', showMeSkills);
