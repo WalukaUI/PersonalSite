a4e7e73246102738a0c8e4b0460467554a692500
WalukaUI <cwaluka@yahoo.com>
2021-03-27 23:30:34 -0700

    java eventlistner

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-let moreAboutMe = document.getElementsByClassName('.aboutButton');
-let mySkills = document.getElementsByClassName('.mySkills');
-let clickHere = document.getElementsByClassName('.clickHere');
-let moreAboutMe = document.getElementsByClassName('.aboutButton');
+
+const input = document.querySelector('#main');
+const inputbut = document.querySelector('#aboutButton');
+
+
+function showMe() {
+  var maineDiv = document.getElementById('main');
+ 
+  if(maineDiv.style.display == '' || maineDiv.style.display == 'none'){
+    maineDiv.style.display = 'block';
+  }
+  else {
+    maineDiv.style.display = 'none';
+  }
+}
+
+inputbut.addEventListener('click', showMe);
+
