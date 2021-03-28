94f145061f8815d924eb2acc48268e32059a5358
WalukaUI <cwaluka@yahoo.com>
2021-03-28 08:51:17 -0700

    eventlistner for MySites

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const inputbutton = document.querySelector('.aboutButton');
-
 function showMe() {
   var maineDiv = document.getElementById('main');
   if(maineDiv.style.display === '' || maineDiv.style.display === 'none'){
@@ -12,8 +11,8 @@ function showMe() {
 }
 inputbutton.addEventListener('click', showMe);
 
-const inputMySkills = document.querySelector('.mySkills');
 
+const inputMySkills = document.querySelector('.mySkills');
 function showMeSkills() {
   var skillsDiv = document.getElementById('skills');
   if(skillsDiv.style.display === '' || skillsDiv.style.display === 'none'){
@@ -24,3 +23,16 @@ function showMeSkills() {
   }
 }
 inputMySkills.addEventListener('click', showMeSkills);
+
+
+const inputMySites = document.querySelector('.toMySites');
+function showMeSites() {
+  var mySitesDiv = document.getElementById('mySites');
+  if(mySitesDiv.style.display === '' || mySitesDiv.style.display === 'none'){
+    mySitesDiv.style.display = 'block';
+  }
+  else {
+    mySitesDiv.style.display = 'none';
+  }
+}
+inputMySites.addEventListener('click', showMeSites);
