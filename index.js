22103dd39d56fcff26792b4e8a6f94585a04ea0f
WalukaUI <cwaluka@yahoo.com>
2021-03-27 22:28:56 -0700

    java script lets

---
+++ index.js
@@ -0,0 +1,4 @@
+let moreAboutMe = document.getElementsByClassName('.aboutButton');
+let mySkills = document.getElementsByClassName('.mySkills');
+let clickHere = document.getElementsByClassName('.clickHere');
+let moreAboutMe = document.getElementsByClassName('.aboutButton');
